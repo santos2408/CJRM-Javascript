d1a7bc7336ca197e72b7a20ba437f84567b40750
Roger Santos <roger.santos36@gmail.com>
2022-06-29 16:53:21 -0300

    :sparkles: adicionada aula 2 da etapa 15

--- Etapa-15/Aula-02/app.js
+++ Etapa-15/Aula-02/app.js
@@ -69,6 +69,182 @@ const roger = new Student('Roger Santos', 'roger.santos36@gmail.com')
 /*
 =============== PROTOTYPES ===============
 
+Como vimos no exemplo anterior, declarar métodos dentro de funções construtoras 
+não é bom para a performance da aplicação, isso porque quando adicionamos métodos 
+dentro de uma função construtora, esse método será declarado em cada novo objeto 
+que a função construtorar criar. Mesmo que os métodos sejam idênticos, eles irão 
+ocupar dois espaços diferentes na memória, pois cada um vai ocupar um espaço 
+diferente. Com isso a aplicação irá consumir mais memória do que necessário. Mas 
+existe uma forma de evitar isso.
 
+Em javascript todo novo objeto que é criado herda propriedades e métodos do 
+seu prototype, que é um objeto do qual um novo objeto que você criou vai hardar 
+as propriedades e métodos, ou seja, é o objeto que armazena as propriedades e 
+métodos que são herdados pelo novo objeto que criarmos.
+
+Perceba que um array possui apenas algumas propriedades, mas como conseguimos 
+acessar o método 'includes' dele ? Isso porque o método 'includes' existe dentro 
+da propriedade prototype e quando invocamos esse método no array, ele verifica 
+se esse método existe no array, se nao existir a engine do JS vai procurar 
+automaticamente dentro da propriedade prototype do array.
+
+Agora repare que, métodos declarados dentro de uma função construtora são 
+armazenados no próprio objeto, ou seja, o método está junto com as propriedades. 
+E isso acontecerá para cada objeto criado e mesmo sendo iguais estão em espaços 
+diferentes na memória.
+
+Para evitarmos isso é possível declararmos o método uma vez, armazenarmos ele 
+em apenas um espaço na memória e fazer com que todos os objetos que sejam criados 
+pela função construtora consiga acesar o método através da referência dele. Para 
+isso temos que armazená-lo dentro da propriedade prototype do objeto.
+
+Vale lembrar que cada tipo de objeto contém um prototype, as vezes com métodos e 
+propriedades diferentes para aquele tipo de objeto. Mas não significa que o 
+prototype é criado para cada tipo de objeto, na verdade o prototype é um objeto 
+único, nativo da linguagem que é referenciado quando é criado um novo objeto. 
+Isso significa que se tivermos dois arrays diferentes na memória, eles estarão 
+apontando para o mesmo prototype do objeto Array na memória, isso diminui o 
+consumo de memória. Portanto cada tipo de objeto tem seus métodos armazenados no 
+seu prototype.
+
+Faremos isso com a nossa função construtora, vamos adicionar os métodos dentro do 
+prototype do objeto criado, invés de adicionar dentro da própria função construtora.
+
+A propriedade prototype é tanto um getter quanto setter, ou seja, nós conseguimos 
+obter dados dele, ou seja, métodos e também conseguimos inserir dados nele, ou seja, 
+métodos.
+
+*/
+
+const Student = (name, email) => {
+  this.name = name
+  this.email = email
+}
+
+Student.prototype.login = function () { // setando método no prototype de Student / setter
+  return `${this.name} fez login.`
+}
+
+Student.prototype.comment = function () {
+  return `${this.name} comentou no post`
+}
+
+const roger = new Student('Roger Santos', 'roger.santos36@gmail.com')
+const alessandra = new Student('Alessandra Carvalho', 'alessandra.pigmentar@gmail.com')
+
+[].includes() === [].__prototype__.includes() // true
+
+Array1.__prototype__ === Array2.__prototype__ // true
+
+roger.login === alessandra.login // true
+roger.comment === alessandra.comment // true
+// método login e comment dentro do prototype de cada objeto
+// referenciando o mesmo método na memória
+
+/*
+
+  Pode ter surgido a seguinte dúvida: Você deve ter reparado que para declararmos 
+  os métodos dentro da propriedade prototype nós temos que declarar fora da 
+  função construtora, sendo assim, quando trabalhando com classes, também é 
+  necessário fazer isso? Não!
+
+  Quando estamos trabalhando com classes, os métodos declarados dentro da classe 
+  são automaticamente inseridos dentro do prototype do objeto sem precisarmos 
+  declarar diretamente os métodos no prototype.
+
+*/
+
+class Student {
+  constructor (name, email) {
+    this.name = name
+    this.email = email
+  }
+
+  comment () { // setado dentro do prototype de Student automaticamente
+    return `${this.name} comentou`
+  }
+}
+
+/*
+
+  Agora, quando não declaramos um método de uma função construtora dentro de um 
+  prototype e sim diretamente na função construtora, esse método será chamado de 
+  método estático, ou seja, ele é invocado a partir do construtor do objeto e não 
+  a partir de uma referência dentro do prototype.
+
+  O método estático será útil quando precisarmos declarar um método utilitário,
+  que é um método que pode ser útil em situações frequentes na aplicação. Por 
+  exemplo, o método from() de Array é um método estático.
+
+*/
+
+// método estático, ou seja, sendo inserido dentro da função construtora
+Student.formatToDatabase = function (aString) { // function declartion anônima
+  return aString
+    .toUpperCase()
+    .replaceAll(' ', '_')
+}
+
+/*
+  O método estático não é visível nem dentro do objeto e nem dentro da propriedade 
+  prototype, apenas na função construtora. Para visualizarmos ele na função com 
+  sintaxe de objeto, podemos usar o método 'dir' do console.
+
+  Agora, nós também podemos declarar um método estático dentro de uma classe, 
+  para isso usamos a notação 'static' para informar que será um método desse tipo.
+*/
+
+class Student {
+  constructor (name, email) {
+    this.name = name
+    this.email = email
+  }
+
+  comment () { // setado dentro do prototype de Student automaticamente
+    return `${this.name} comentou`
+  }
+
+  static formatToDatabase (aString) { // function declartion com nome
+    return aString
+      .toUpperCase()
+      .replaceAll(' ', '_')
+  }
+}
+
+/*
+ 
+  Perceba que, quando visualizamos esse método estático dentro da classe ele 
+  possui uma propriedade 'name' que armazena o nome do método, mas quando se 
+  é com a função construtora, essa propriedade 'name' armazena uma string vazia, 
+  isso porque, quando declaramos um método estático com abreviação de function 
+  declaration, como fizemos dentro da classe, a propriedade 'name' recebe o nome 
+  do método que foi declarado.
+
+  Mas quando declaramos o método estático com a sintaxe de function declartion 
+  completa, ou seja, anônima, estamos explicitando que a função não constém nome.
+
+  Isso é problemático porque um erro envolvendo uma função anônima pode retornar 
+  uma string em branco como nome da função. Mesmo que o erro indique a linha que 
+  ocorreu, não existe justificativa para trabalhar com function declarion anônima, 
+  a não ser que ela seja atribuída para uma const ou let, pois quando declaramos 
+  uma função anônima com const ou let, seu nome é atribuído normalmente.
+
+  Método usando function declaration anônima não é recomendado!
+
+*/
+
+Student.formatToDatabase = function (aString) { // function declartion anônima / não recomendado
+  // ...
+}
+
+Student.formatToDatabase = function formatToDatabase (aString) { // function declartion com nome / recomendado
+  // ...
+}
+
+/*
+
+  Apesar disso tudo, métodos estáticos podem ser substituídos por simples funções,
+  o que é particularmente mais simples e claro do que classes, métodos estáticos 
+  e outras features da orientação a objetos.
 
 */
